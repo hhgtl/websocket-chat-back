181f4f04ef707f7ebba3a1f1d0857f04d5d1dec0
Petro Sahal(hhgtl) <petrosahal66@gmail.com>
2025-05-09 21:35:24 +0300

    feat: add type

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,21 @@
-import express from 'express';
-import { createServer } from 'node:http';
-import { Server } from 'socket.io';
+import express, { Request, Response } from 'express';
+import { createServer, IncomingMessage, ServerResponse } from 'node:http';
+import { Server, Socket } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-
-app.get('/', (req, res) => {
+// Типізація обробника для маршруту
+app.get('/', (req: Request, res: Response): void => {
     res.send('Hello');
 });
 
-io.on('connection', (socket) => {
+// Типізація події підключення
+io.on('connection', (socket: Socket): void => {
     console.log('a user connected');
 });
+
 const PORT = process.env.PORT || 3009;
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
