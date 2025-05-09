bafe94fc2b169e80d430856190439d57f670b693
Petro Sahal(hhgtl) <petrosahal66@gmail.com>
2025-05-09 21:39:17 +0300

    fix: app types

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,21 @@
 import express, { Request, Response } from 'express';
-import { createServer, IncomingMessage, ServerResponse } from 'node:http';
+import { createServer } from 'node:http';
 import { Server, Socket } from 'socket.io';
+
+
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-// Типізація обробника для маршруту
+
 app.get('/', (req: Request, res: Response): void => {
     res.send('Hello');
 });
 
-// Типізація події підключення
 io.on('connection', (socket: Socket): void => {
     console.log('a user connected');
 });
-
 const PORT = process.env.PORT || 3009;
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
