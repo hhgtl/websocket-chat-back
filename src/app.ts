eb60a01598a5aa6a536687b75bb16e4aa9efbcb3
Petro Sahal(hhgtl) <petrosahal66@gmail.com>
2025-05-10 00:22:06 +0300

    feat: changes cors

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,32 @@
 import express, { Request, Response } from 'express';
 import { createServer } from 'node:http';
 import { Server, Socket } from 'socket.io';
+import cors from 'cors';
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+}));
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+        credentials: true,
+    },
+});
 
 app.get('/', (req: Request, res: Response): void => {
-    res.send('Hellos');
+    res.send('Hello');
 });
 
 io.on('connection', (socket: Socket): void => {
     console.log('a user connected');
 });
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
